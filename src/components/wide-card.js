18f858291f5d32bce8ac2e9b99cf194da9e35f08
A-Maged <a.maged99@outlook.com>
2018-03-18 23:48:01 +0200

    .

--- src/components/wide-card.js
+++ src/components/wide-card.js
@@ -6,13 +6,17 @@ const WideCard = (props) => {
         <div className="post">
 
             <div className="thumbnail">
-                <img src={props.imgUrl} alt="" />
+                <Link to={"/post/" + props.slug}>
+                    <img src={props.imgUrl} alt="" />
+                </Link>
             </div>
 
             <div className="details">
                 <div className="vertical-align">
                     <h4 className="title">
-                        <Link to={"/post/" + props.slug}>{props.title}</Link>
+                        <Link to={"/post/" + props.slug}>
+                            {props.title}
+                        </Link>
                     </h4>
                     <p className="excerpt">
                          {props.excerpt}
