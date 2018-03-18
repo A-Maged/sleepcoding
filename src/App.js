5b38263e3517f505b51aa4a799fa45bf6577f997
A-Maged <a.maged99@outlook.com>
2018-03-18 04:29:01 +0200

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* libs */
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom"; // using HashRouter instead of BrowserRouter cause it works fine with github pages
 
 /* redux */
 import store from './redux/store';
